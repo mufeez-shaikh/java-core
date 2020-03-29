4bc6acf3138e2fc70c042439b7255afe49b5a5ce
mufiz-mac <mufeez.shaikh@outlook.com>
2020-03-28 21:39:33 -0600

    thread concepts

---
+++ src/main/java/com/mufiz/concurrent/SimpleThreadPool.java
@@ -0,0 +1,11 @@
+package com.mufiz.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+    }
+}
